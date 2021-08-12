99dbcda086362f5fe2ef850d4fe034681b18e71c
Khanh Le <khanhle@Khanhs-MacBook-Pro.local>
2021-08-12 17:56:35 +0700

    Add to git

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import 'tailwindcss/tailwind.css'
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+import Head from "next/head";
+import { AppWrapper } from '../lib/context';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  let sharedState = {}
+
+  return (
+    <AppWrapper>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </AppWrapper>
+  )
 }
 
 export default MyApp
